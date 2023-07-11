9eb04ab25931541d7f587dfec7f23fed06648ba0
Kirill <kirdro@yandex.ru>
2023-07-11 19:43:04 +0700

    fix

---
+++ src/app/templ/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+    return (
+        <main >
+            <div >
+                <p>
+                    Get started by editing&nbsp;
+                    <code >src/app/page.tsx</code>
+                </p>
+
+            </div>
+
+
+
+
+        </main>
+    )
+}

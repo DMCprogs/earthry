1a3e51a69fbbec7511a6c4f2bf32b6c385431162
Homiak332 <Stasmaksimov2003@gmail.com>
2023-07-14 12:18:26 +0700

    add cardblog link, add props styles custombutton

--- src/app/components/blog/BlogCard.tsx
+++ src/app/components/blog/BlogCard.tsx
@@ -1,31 +1,51 @@
+"use client";
 import { FC } from "react";
-import { 
+import { useRouter } from 'next/navigation';
+
+import {
   BlogCardContainerSC,
   BlogCardImageSC,
   BlogCardTitleSC,
   DivProductContentSC,
   BlogCardDescriptionSC
- } from "./styles.BlogCard";
+} from "./styles.BlogCard";
+import ButtonWrapper from "../custom_button";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import Link from "next/link";
 
- type BlogCardProps = {
-  img:any;
+type BlogCardProps = {
+  img: any;
 }
-const BlogCard: FC<BlogCardProps> = ({img}) => {
+const BlogCard: FC<BlogCardProps> = ({ img }) => {
+  const router = useRouter();
+
+  const styles = {
+    justifySelf: 'end',
+  }
   return (
 
 
     <BlogCardContainerSC>
-      <BlogCardImageSC img={img}/>
+      <BlogCardImageSC img={img} />
       <DivProductContentSC>
-      <BlogCardTitleSC>Lorem ipsum dolor sit amet, consectetur adipiscing elit</BlogCardTitleSC>
-      <BlogCardDescriptionSC>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</BlogCardDescriptionSC>
-
+        <BlogCardTitleSC>Lorem ipsum dolor sit amet, consectetur adipiscing elit</BlogCardTitleSC>
+        <BlogCardDescriptionSC>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</BlogCardDescriptionSC>
+        <ButtonWrapper
+          styles={styles}
+          directionRadius="center"
+          primary={true}
+          height={50}
+          width={50}
+          onClick={() => router.push('/')}
+        >
+          <HiOutlineArrowNarrowRight size={"1.3rem"} />
+        </ButtonWrapper>
       </DivProductContentSC>
 
-      </BlogCardContainerSC>
-    );
+    </BlogCardContainerSC>
+  );
 
-    
+
 };
 
 export default BlogCard;

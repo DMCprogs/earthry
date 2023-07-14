b7da3515c0983a247945c29f5fc52b6b420a12f2
Gordin Ruslan <gordinruslan009@gmail.com>
2023-07-14 23:22:34 +0700

    fix

--- src/app/tokenomics/page.tsx
+++ src/app/tokenomics/page.tsx
@@ -14,7 +14,7 @@ import {
   ImagePlanetTokenSC,
   DivWrapImgTitleSC,
 } from "./styles.tokenomics";
-import Image from "../images/chart.png";
+import Image from "../images/chart.svg";
 import Image2 from "../images/planet_token.svg";
 import { SpanTextSC } from "./styles.tokenomics";
 import ButtonWrapper from "../components/custom_button";

def28c45aab36576c8c9f8ac7dee6126c59edacf
Gordin Ruslan <gordinruslan009@gmail.com>
2023-07-14 17:59:57 +0700

    fix delete

--- src/app/tokenomics/page.tsx
+++ src/app/tokenomics/page.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import {
   DivWrapperCellImageSC,
   DivWrapperContentSC,
-  DivBoxText1SC,
   H4SC,
   TitleTokenomicsSC,
   DivBoxOptionsTokensSC,
